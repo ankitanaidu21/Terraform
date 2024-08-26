e3022142f5b337661d56cf522252789de3da71e7
Ankita_Naidu21 <76999548+ankitanaidu21@users.noreply.github.com>
2024-08-26 10:27:25 +0530

    Delete main.tf

--- main.tf
+++
@@ -1,9 +0,0 @@
-provider "aws" {
-  region = "us-east-1"
-}
-
-resource "aws_instance" "this" {
-  ami                     = "ami-02c21308fed24a8ab"
-  instance_type           = "t2.micro"
-  key_name = "demo"
-}
